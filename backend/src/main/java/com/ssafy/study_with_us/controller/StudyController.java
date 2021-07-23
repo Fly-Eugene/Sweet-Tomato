d0639fd80c8401aefab5d6cde7c1809dc649efbe
김종혁 <alb7979s@naver.com>
2021-07-23 16:47:58 +0900

    feat: studyController 생성, studyDto 작성

--- backend/src/main/java/com/ssafy/study_with_us/controller/StudyController.java
+++ backend/src/main/java/com/ssafy/study_with_us/controller/StudyController.java
@@ -1,8 +1,16 @@
 package com.ssafy.study_with_us.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/study")
 public class StudyController {
 
+    @PostMapping
+    public Object create() {
+        return null;
+    }
 }
