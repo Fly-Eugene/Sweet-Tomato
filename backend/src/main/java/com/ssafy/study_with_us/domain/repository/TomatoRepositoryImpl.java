825560d7f477b1f51b172ec75ed274d213ee9265
김종혁 <alb7979s@naver.com>
2021-08-04 14:01:55 +0900

    feat: 토마토 기능 구조 작성

---
+++ backend/src/main/java/com/ssafy/study_with_us/domain/repository/TomatoRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.ssafy.study_with_us.domain.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TomatoRepositoryImpl implements TomatoRepositoryCustom {
+
+}
