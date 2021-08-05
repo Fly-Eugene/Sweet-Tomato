df35225824e04c60125e91f3aeb3fedf630f0170
김종혁 <alb7979s@naver.com>
2021-08-05 10:58:48 +0900

    feat: 토마토 추가 기능

--- backend/src/main/java/com/ssafy/study_with_us/domain/repository/TomatoRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/study_with_us/domain/repository/TomatoRepositoryImpl.java
@@ -1,7 +1,15 @@
 package com.ssafy.study_with_us.domain.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.study_with_us.domain.entity.QTomato;
+import com.ssafy.study_with_us.domain.entity.Tomato;
+import com.ssafy.study_with_us.dto.TomatoDto;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+import static com.ssafy.study_with_us.domain.entity.QTomato.*;
 
 @Repository
 public class TomatoRepositoryImpl implements TomatoRepositoryCustom {
@@ -9,5 +17,22 @@ public class TomatoRepositoryImpl implements TomatoRepositoryCustom {
 
     public TomatoRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
         this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    @Override
+    public Tomato addTomato(TomatoDto params) {
+        return jpaQueryFactory.selectFrom(tomato)
+                .where(memberIdEq(params.getMemberId()), studyIdEq(params.getStudyId()), tomatoDateEq(params.getDate()))
+                .fetchOne();
+    }
+
+    private BooleanExpression memberIdEq(Long memberId){
+        return memberId == null ? null : tomato.member.id.eq(memberId);
+    }
+    private BooleanExpression studyIdEq(Long studyId){
+        return studyId == null ? null : tomato.study.id.eq(studyId);
+    }
+    private BooleanExpression tomatoDateEq(LocalDate date){
+        return date == null ? null : tomato.tomatoDate.eq(date);
     }
 }
--- backend/src/main/java/com/ssafy/study_with_us/service/TomatoService.java
+++ backend/src/main/java/com/ssafy/study_with_us/service/TomatoService.java
@@ -1,15 +1,50 @@
 package com.ssafy.study_with_us.service;
 
+import com.ssafy.study_with_us.domain.entity.Tomato;
+import com.ssafy.study_with_us.domain.repository.MemberRepository;
+import com.ssafy.study_with_us.domain.repository.StudyRepository;
 import com.ssafy.study_with_us.domain.repository.TomatoRepository;
+import com.ssafy.study_with_us.dto.TomatoDto;
+import com.ssafy.study_with_us.util.SecurityUtil;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class TomatoService {
     private final TomatoRepository tomatoRepository;
+    private final MemberRepository memberRepository;
+    private final StudyRepository studyRepository;
 
-    public TomatoService(TomatoRepository tomatoRepository) {
+    public TomatoService(TomatoRepository tomatoRepository, MemberRepository memberRepository, StudyRepository studyRepository) {
         this.tomatoRepository = tomatoRepository;
+        this.memberRepository = memberRepository;
+        this.studyRepository = studyRepository;
     }
 
+    //  date type 찾아봐야함
+    public Object addTomato(TomatoDto params){
+        // 여기서 tomato null이면 만들어주고 아니면 count + 1 해줌
+        Tomato tomato = tomatoRepository.addTomato(TomatoDto.builder().memberId(getMemberId())
+                .studyId(params.getStudyId()).date(LocalDate.now()).build());
+        System.out.println("tomato = " + tomato);
+        System.out.println("params = " + params);
+        Tomato result = null;
+        if(tomato == null) {
+            result = tomatoRepository.save(Tomato.builder().tomatoCount(1)
+                    .member(memberRepository.getById(getMemberId())).study(studyRepository.getById(params.getStudyId()))
+                    .tomatoDate(LocalDate.now()).build());
+        } else {
+            result = tomatoRepository.save(Tomato.builder().id(tomato.getId()).tomatoCount(tomato.getTomatoCount()+1)
+                    .member(tomato.getMember()).study(tomato.getStudy())
+                    .tomatoDate(tomato.getTomatoDate()).build());
+        }
+        return result;
+    }
+
+    private Long getMemberId() {
+        String s = SecurityUtil.getCurrentUsername().get();
+        return memberRepository.findByEmail(s).get().getId();
+    }
 
 }
