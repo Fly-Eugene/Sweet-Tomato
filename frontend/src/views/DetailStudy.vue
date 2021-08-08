a8a12fccc51b33b9c2b63f70c196bee46c35063c
이유진 <eujjin@naver.com>
2021-08-08 14:17:10 +0900

    fix : 0809 merge 전 commit

--- frontend/src/views/DetailStudy.vue
+++ frontend/src/views/DetailStudy.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="detail_study_all">
-    <StudyInfo :info='state.info' v-if='Object.keys(state.info).length'/>
+    <StudyInfo />
+    <!-- <StudyInfo :info='state.info' v-if='Object.keys(state.info).length'/> -->
     <TomatoRate/>
     <StudyChart/>
     <StudyComment :studyId='id'/>
@@ -10,8 +11,7 @@
 
 <script>
 import '@/assets/style/detail_study.scss'
-import { onMounted, reactive, ref } from 'vue'
-import $axios from 'axios'
+import { onMounted } from 'vue'
 import { useStore } from 'vuex'
 import StudyInfo from '@/components/DetailStudy/StudyInfo.vue'
 import StudyChart from '@/components/DetailStudy/StudyChart.vue'
@@ -36,37 +36,17 @@ export default {
   },
 
   setup(props) {
-
-    const check = ref(0)
-    const state = reactive({
-      info : {}
-    })
     const store = useStore()
     
     onMounted(() => {
       console.log('onMounted 실행')
-      $axios({
-        method: 'get',
-        url: store.state.server_url + 'study/detail',
-        // url: 'https://localhost:5000/study/detail',
-        params : {
-          id : props.id
-        }
-      })
-      .then(res => {
-        console.log(res.data.data)
-        state.info = res.data.data
-      })
-      .catch(err => {
-        console.log(err)
-      }),
-
+      store.dispatch('getStudyInfo', props.id)
+      store.dispatch('getStudyImg', props.id )
       store.dispatch('getComments', props.id)
     })
 
     return {
-      state,
-      check
+      // state,
     }
   }
 
--- frontend/src/views/Room.vue
+++ frontend/src/views/Room.vue
@@ -112,188 +112,8 @@
 //   }
 // }
 
-var kurentoUtils = require('kurento-utils');
-
-var ws = new WebSocket('wss://localhost:5000/groupcall')
-var name = ''
-var participants = []
-
-ws.onmessage = function(message) {
-    var parsedMessage = JSON.parse(message.data);
-    console.info('Received message: ' + message.data);
-
-    switch (parsedMessage.id) {
-        case 'existingParticipants':
-            onExistingParticipants(parsedMessage);
-            break;
-        case 'newParticipantArrived':
-            onNewParticipant(parsedMessage);
-            break;
-        case 'participantLeft':
-            onParticipantLeft(parsedMessage);
-            break;
-        case 'receiveVideoAnswer':
-            receiveVideoResponse(parsedMessage);
-            break;
-        case 'iceCandidate':
-            participants[parsedMessage.name].rtcPeer.addIceCandidate(parsedMessage.candidate, function (error) {
-                if (error) {
-                console.error("Error adding candidate: " + error);
-                return;
-                }
-            });
-            break;
-        default:
-            console.error('Unrecognized message', parsedMessage);
-    }
-}
-function onExistingParticipants(msg) {
-    var constraints = {
-        audio : true,
-        video : {
-            mandatory : {
-                maxWidth : 320,
-                maxFrameRate : 15,
-                minFrameRate : 15
-            }
-        }
-    }
-    var participant = new Participant(name);
-    participants[name] = participant;
-    var video = participant.getVideoElement();
-
-    var options = {
-        localVideo: video,
-        mediaConstraints: constraints,
-        onicecandidate: participant.onIceCandidate.bind(participant)
-        }
-    participant.rtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerSendonly(options,
-        function (error) {
-        if(error) {
-            return console.error(error);
-        }
-        this.generateOffer (participant.offerToReceiveVideo.bind(participant));
-    });
-
-    msg.data.forEach(receiveVideo);
-}
-function Participant(param) {
-    name = param;
-    var container = document.createElement('div');
-    container.className = isPresentMainParticipant() ? PARTICIPANT_CLASS : PARTICIPANT_MAIN_CLASS;
-    container.id = name;
-    var span = document.createElement('span');
-    var video = document.createElement('video');
-    var rtcPeer; // eslint-disable-line no-unused-vars
-
-    container.appendChild(video);
-    container.appendChild(span);
-    container.onclick = switchContainerClass;
-    document.getElementById('participants').appendChild(container);
-
-    span.appendChild(document.createTextNode(name));
-
-    video.id = 'video-' + name;
-    video.autoplay = true;
-    video.controls = false;
 
 
-    this.getElement = function() {
-        return container;
-    }
-
-    this.getVideoElement = function() {
-        return video;
-    }
-
-    function switchContainerClass() {
-        if (container.className === PARTICIPANT_CLASS) {
-            var elements = Array.prototype.slice.call(document.getElementsByClassName(PARTICIPANT_MAIN_CLASS));
-            elements.forEach(function(item) {
-                    item.className = PARTICIPANT_CLASS;
-                });
-
-                container.className = PARTICIPANT_MAIN_CLASS;
-            } else {
-            container.className = PARTICIPANT_CLASS;
-        }
-    }
-
-    function isPresentMainParticipant() {
-        return ((document.getElementsByClassName(PARTICIPANT_MAIN_CLASS)).length != 0);
-    }
-
-    this.offerToReceiveVideo = function(error, offerSdp, wp){// eslint-disable-line no-unused-vars
-        if (error) return console.error ("sdp offer error")
-        console.log('Invoking SDP offer callback function');
-        var msg =  { id : "receiveVideoFrom",
-                sender : name,
-                sdpOffer : offerSdp
-            };
-        sendMessage(msg);
-    }
-
-
-    this.onIceCandidate = function (candidate, wp) {// eslint-disable-line no-unused-vars
-        console.log("Local candidate" + JSON.stringify(candidate));
-
-        var message = {
-            id: 'onIceCandidate',
-            candidate: candidate,
-            name: name
-        };
-        sendMessage(message);
-    }
-
-    Object.defineProperty(this, 'rtcPeer', { writable: true});
-
-    this.dispose = function() {
-        console.log('Disposing participant ' + this.name);
-        this.rtcPeer.dispose();
-        container.parentNode.removeChild(container);
-    };
-}
-       
-
-function receiveVideo(sender) {
-    var participant = new Participant(sender);
-    participants[sender] = participant;
-    var video = participant.getVideoElement();
-
-    var options = {
-    remoteVideo: video,
-    onicecandidate: participant.onIceCandidate.bind(participant)
-    }
-
-    participant.rtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
-            function (error) {
-            if(error) {
-                return console.error(error);
-            }
-            this.generateOffer (participant.offerToReceiveVideo.bind(participant));
-    });
-}
-function onParticipantLeft(request) {
-    console.log('Participant ' + request.name + ' left');
-    var participant = participants[request.name];
-    participant.dispose();
-    delete participants[request.name];
-}
-
-function sendMessage(message) {
-    var jsonMessage = JSON.stringify(message);
-    console.log('Sending message: ' + jsonMessage);
-    ws.send(jsonMessage);
-}
-function onNewParticipant(request) {
-    receiveVideo(request.name);
-}
-
-function receiveVideoResponse(result) {
-    participants[result.name].rtcPeer.processAnswer (result.sdpAnswer, function (error) {
-        if (error) return console.error (error);
-    });
-}
 // function callResponse(message) {
 //     if (message.response != 'accepted') {
 //         console.info('Call not accepted by peer. Closing call');
@@ -304,23 +124,222 @@ function receiveVideoResponse(result) {
 //         });
 //     }
 // }
+import { onMounted } from 'vue'
+// import '@/assets/style/room.scss'
+
 const PARTICIPANT_MAIN_CLASS = 'participant main';
 const PARTICIPANT_CLASS = 'participant';
 
 export default { 
     name: "RoomTest",
     setup(){
-       
-    },
-    mounted(){
-        window.addEventListener('beforeunload', this.unLoadEvent);
-        
-    },
-    methods: {
-        unLoadEvent: function(){
+
+        var kurentoUtils = require('kurento-utils');
+        var ws = new WebSocket('wss://localhost:5000/groupcall')
+        var name = ''
+        var participants = []
+
+        ws.onmessage = function(message) {
+            var parsedMessage = JSON.parse(message.data);
+            console.info('Received message: ' + message.data);
+
+            switch (parsedMessage.id) {
+                case 'existingParticipants':
+                    onExistingParticipants(parsedMessage);
+                    break;
+                case 'newParticipantArrived':
+                    onNewParticipant(parsedMessage);
+                    break;
+                case 'participantLeft':
+                    onParticipantLeft(parsedMessage);
+                    break;
+                case 'receiveVideoAnswer':
+                    receiveVideoResponse(parsedMessage);
+                    break;
+                case 'iceCandidate':
+                    participants[parsedMessage.name].rtcPeer.addIceCandidate(parsedMessage.candidate, function (error) {
+                        if (error) {
+                        console.error("Error adding candidate: " + error);
+                        return;
+                        }
+                    });
+                    break;
+                default:
+                    console.error('Unrecognized message', parsedMessage);
+            }
+        }
+
+        function onExistingParticipants(msg) {
+            var constraints = {
+                audio : true,
+                video : {
+                    mandatory : {
+                        maxWidth : 320,
+                        maxFrameRate : 15,
+                        minFrameRate : 15
+                    }
+                }
+            }
+            var participant = new Participant(name);
+            participants[name] = participant;
+            var video = participant.getVideoElement();
+
+            var options = {
+                localVideo: video,
+                mediaConstraints: constraints,
+                onicecandidate: participant.onIceCandidate.bind(participant)
+                }
+            participant.rtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerSendonly(options,
+                function (error) {
+                if(error) {
+                    return console.error(error);
+                }
+                this.generateOffer (participant.offerToReceiveVideo.bind(participant));
+            });
+
+            msg.data.forEach(receiveVideo);
+        }
+
+        function Participant(param) {
+            name = param;
+            var container = document.createElement('div');
+            container.className = isPresentMainParticipant() ? PARTICIPANT_CLASS : PARTICIPANT_MAIN_CLASS;
+            container.id = name;
+            var span = document.createElement('span');
+            var video = document.createElement('video');
+            var rtcPeer; // eslint-disable-line no-unused-vars
+
+            container.appendChild(video);
+            container.appendChild(span);
+            container.onclick = switchContainerClass;
+            document.getElementById('participants').appendChild(container);
+
+            span.appendChild(document.createTextNode(name));
+
+            video.id = 'video-' + name;
+            video.autoplay = true;
+            video.controls = false;
+
+
+            this.getElement = function() {
+                return container;
+            }
+
+            this.getVideoElement = function() {
+                return video;
+            }
+
+            function switchContainerClass() {
+                if (container.className === PARTICIPANT_CLASS) {
+                    var elements = Array.prototype.slice.call(document.getElementsByClassName(PARTICIPANT_MAIN_CLASS));
+                    elements.forEach(function(item) {
+                            item.className = PARTICIPANT_CLASS;
+                        });
+
+                        container.className = PARTICIPANT_MAIN_CLASS;
+                    } else {
+                    container.className = PARTICIPANT_CLASS;
+                }
+            }
+
+            function isPresentMainParticipant() {
+                return ((document.getElementsByClassName(PARTICIPANT_MAIN_CLASS)).length != 0);
+            }
+
+            this.offerToReceiveVideo = function(error, offerSdp, wp){// eslint-disable-line no-unused-vars
+                if (error) return console.error ("sdp offer error")
+                console.log('Invoking SDP offer callback function');
+                var msg =  { id : "receiveVideoFrom",
+                        sender : name,
+                        sdpOffer : offerSdp
+                    };
+                sendMessage(msg);
+            }
+
+
+            this.onIceCandidate = function (candidate, wp) {// eslint-disable-line no-unused-vars
+                console.log("Local candidate" + JSON.stringify(candidate));
+
+                var message = {
+                    id: 'onIceCandidate',
+                    candidate: candidate,
+                    name: name
+                };
+                sendMessage(message);
+            }
+
+            Object.defineProperty(this, 'rtcPeer', { writable: true});
+
+            this.dispose = function() {
+                console.log('Disposing participant ' + this.name);
+                this.rtcPeer.dispose();
+                container.parentNode.removeChild(container);
+            };
+        }
+            
+
+        function receiveVideo(sender) {
+            var participant = new Participant(sender);
+            participants[sender] = participant;
+            var video = participant.getVideoElement();
+
+            var options = {
+            remoteVideo: video,
+            onicecandidate: participant.onIceCandidate.bind(participant)
+            }
+
+            participant.rtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
+                    function (error) {
+                    if(error) {
+                        return console.error(error);
+                    }
+                    this.generateOffer (participant.offerToReceiveVideo.bind(participant));
+            });
+        }
+
+        function onParticipantLeft(request) {
+            console.log('Participant ' + request.name + ' left');
+            var participant = participants[request.name];
+            participant.dispose();
+            delete participants[request.name];
+        }
+
+        function sendMessage(message) {
+            var jsonMessage = JSON.stringify(message);
+            console.log('Sending message: ' + jsonMessage);
+            ws.send(jsonMessage);
+        }
+
+        function onNewParticipant(request) {
+            receiveVideo(request.name);
+        }
+
+        function receiveVideoResponse(result) {
+            participants[result.name].rtcPeer.processAnswer (result.sdpAnswer, function (error) {
+                if (error) return console.error (error);
+            });
+        }
+
+        // function callResponse(message) {
+        //     if (message.response != 'accepted') {
+        //         console.info('Call not accepted by peer. Closing call');
+        //         stop();
+        //     } else {
+        //         webRtcPeer.processAnswer(message.sdpAnswer, function (error) {
+        //             if (error) return console.error (error);
+        //         });
+        //     }
+        // }
+
+        onMounted(() => {
+            window.addEventListener('beforeunload', this.unLoadEvent);
+        })
+
+        const unLoadEvent = function(){
             //ws.close;
-        },
-        register: function(){
+        }
+
+        const register = function(){
             name = document.getElementById('name').value;
             var room = document.getElementById('roomName').value;
 
@@ -333,9 +352,10 @@ export default {
                 name : name,
                 room : room,
             }
-            sendMessage(message);
-        },
-        leaveRoom: function() {
+            sendMessage(message);            
+        }
+
+        const leaveRoom = function(){
             sendMessage({
                 id : 'leaveRoom'
             });
@@ -348,9 +368,19 @@ export default {
             document.getElementById('room').style.display = 'none';
 
             ws.close();
-        },
+        }
+
+
+
         
-    }
+        return {
+            unLoadEvent,
+            register,
+            leaveRoom,
+        }
+       
+    },
+
 }
 </script>
 
