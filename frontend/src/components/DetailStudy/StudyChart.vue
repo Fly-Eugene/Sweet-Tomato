14252c520f07c803ac9add5712fba928c0426811
hyeyemin <wlalsdns@naver.com>
2021-08-11 17:18:06 +0900

    feat: tomatomap 구현중

--- frontend/src/components/DetailStudy/StudyChart.vue
+++ frontend/src/components/DetailStudy/StudyChart.vue
@@ -1,5 +1,7 @@
 <template>
   <section class="chart_section">
+    <button @click="calculateDate">test</button>
+    <button @click="check">check</button>
     <header class="chart_header">토마토 밭</header>
     <apexchart type="heatmap" height="350" width="1000" :options="chartOptions" :series="series"></apexchart>
   </section>
@@ -8,11 +10,72 @@
 <script>
 import '@/assets/style/DetailStudy/study_chart.scss'
 // import VueApexCharts from 'vue3-apexcharts'
-
+import { useStore } from 'vuex'
 export default {
   name: 'StudyChart',
-
   setup() {
+    const store = useStore()
+
+    function currentDate(){
+      var date = new Date();
+      var month = leadingZeros(date.getMonth() + 1, 2);
+      var today = date.getFullYear() + "-" + month + "-" + leadingZeros(date.getDate(),2);
+      console.log(today);
+      return today;
+    }
+
+    function leadingZeros(n, digits){
+      var zero = '';
+      n = n.toString();
+      if(n.length < digits){
+        for(var i = 0; i < digits - n.length; i++)
+          zero += '0';
+      }
+      return zero + n;
+    }
+    function calculateDate(){
+      var tomatoes = store.state.tomatoes;
+      var today = new Date(currentDate());
+      for(var i = 0; i < tomatoes.length; i++){
+          var day1 = new Date(tomatoes[i].date);
+          var dateDiff = Math.ceil((today - day1) / (1000*3600*24));
+          console.log(dateDiff);
+          var temp = dateDiff % 7;
+          switch(temp){
+            case 0:
+                data1[Math.ceil(dateDiff/7)] = tomatoes[i].count;
+                break;
+            case 1:
+                data2[Math.ceil(dateDiff/7)] = tomatoes[i].count;
+                break;
+            case 2:
+                data3[Math.ceil(dateDiff/7)] = tomatoes[i].count;
+                break;
+            case 3:
+                data4[Math.ceil(dateDiff/7)] = tomatoes[i].count;
+                break;
+            case 4:
+                data5[Math.ceil(dateDiff/7)] = tomatoes[i].count;
+                break;
+            case 5:
+                data6[Math.ceil(dateDiff/7)] = tomatoes[i].count;
+                break;
+            case 6:
+                data7[Math.ceil(dateDiff/7)] = tomatoes[i].count;
+                break;
+            
+          }
+      }
+    }
+    function check(){
+      console.log(data1);
+      console.log(data2);
+      console.log(data3);
+      console.log(data4);
+      console.log(data5);
+      console.log(data6);
+      console.log(data7);
+    }
     function generateData(cnt, min, max) {
       let i
       const res = []
@@ -23,16 +86,24 @@ export default {
       }
       return res; //최댓값도 포함, 최솟값도 포함
     }
+    
+    const data1 = []
+    const data2 = []
+    const data3 = []
+    const data4 = []
+    const data5 = []
+    const data6 = []
+    const data7 = []
 
     const series = [
       { name: 'Metric1',
-        data: generateData(18, 0, 90)
+        data: data1
       },
       { name: 'Metric2',
-        data: generateData(18, 0, 90)
+        data: data2
       },
       { name: 'Metric3',
-        data: generateData(18, 0, 90)
+        data: data3
       },
       { name: 'Metric4',
         data: generateData(18, 0, 90)
@@ -68,7 +139,9 @@ export default {
 
     return {
       series,
-      chartOptions
+      chartOptions,
+      calculateDate,
+      check
     }
   }
 }
