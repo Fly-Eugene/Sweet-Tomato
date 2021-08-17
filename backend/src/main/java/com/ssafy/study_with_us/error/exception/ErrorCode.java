07c0d1eb60420ecee583e3f2da1ffc40f5263fd8
김종혁 <alb7979s@naver.com>
2021-08-17 13:20:12 +0900

    feat: 프로필 예외처리

--- backend/src/main/java/com/ssafy/study_with_us/error/exception/ErrorCode.java
+++ backend/src/main/java/com/ssafy/study_with_us/error/exception/ErrorCode.java
@@ -19,7 +19,11 @@ public enum ErrorCode {
     FILE_FORMAT_ERROR(400, "M002", "파일 형식이 맞지 않습니다."),
     EMAIL_NOT_FOUNDED(400, "M003", "이메일을 찾을 수 없습니다."),
 
+    // Profile
+    PROFILE_NOT_FOUNDED(400, "P001", "프로필이 존재하지 않습니다."),
 
+    // File
+    FILE_NOT_FOUNDED(400, "F001", "파일이 존재하지 않습니다."),
     ;
     private final String code;
     private final String message;
